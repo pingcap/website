a547d41197e5969450c572f03944f431202a3c6a
Yue Yang <g1enyy0ung@gmail.com>
2020-02-11 17:38:56 +0800

    chore: update Blogs component

--- src/components/blogs.js
+++ src/components/blogs.js
@@ -1,9 +1,13 @@
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import React, { useState, useEffect } from 'react'
+
 import BlogHeader from '../components/blogHeader'
 import BlogTags from '../components/blogTags'
 import Layout from '../components/layout'
 import Pagination from '../components/pagination'
-import React from 'react'
+import PostFromUs from '../components/postFromUs'
 import SEO from '../components/seo'
+import Socials from '../components/socials'
 
 const Blogs = ({
   data,
@@ -13,13 +17,46 @@ const Blogs = ({
   isCategoryPage,
 }) => {
   const blogs = data.allMarkdownRemark.edges
-  const { currentPage, numPages } = pageContext
+  const {
+    currentPage,
+    numPages,
+    tag: currentTag,
+    category: currentCategory,
+  } = pageContext
 
   const title = isTagPage
     ? pageContext.tag
     : isCategoryPage
     ? pageContext.category
     : 'Blogs'
+
+  const query = useStaticQuery(graphql`
+    query {
+      categories: allMarkdownRemark {
+        group(field: frontmatter___categories) {
+          category: fieldValue
+        }
+      }
+      tags: allMarkdownRemark {
+        group(field: frontmatter___tags) {
+          tag: fieldValue
+        }
+      }
+    }
+  `)
+
+  const categories = query.categories.group.map(i => i.category)
+  const tags = query.tags.group.map(i => i.tag)
+
+  const [showCategories, setShowCategories] = useState(true)
+
+  useEffect(() => {
+    if (isTagPage) {
+      setShowCategories(false)
+    }
+  }, [isTagPage])
+
+  const handleShowCategories = bool => _ => setShowCategories(bool)
 
   return (
     <Layout>
@@ -43,7 +80,64 @@ const Blogs = ({
                   numPages={numPages}
                 />
               </div>
-              <div className="column is-4"></div>
+              <div className="column is-4 right-column">
+                <div className="main">
+                  <PostFromUs />
+                  <div className="follow-us">
+                    <div className="title is-7">Follow to Join Us!</div>
+                    <div className="socials">
+                      <Socials />
+                    </div>
+                  </div>
+                  <div className="categories-and-tags">
+                    <div className="titles">
+                      <div
+                        role="button"
+                        tabIndex={0}
+                        className={`title is-7 categories-title${
+                          showCategories ? ' active' : ''
+                        }`}
+                        onClick={handleShowCategories(true)}
+                        onKeyDown={handleShowCategories(true)}
+                      >
+                        Categories
+                      </div>
+                      <div
+                        role="button"
+                        tabIndex={0}
+                        className={`title is-7${
+                          !showCategories ? ' active' : ''
+                        }`}
+                        onClick={handleShowCategories(false)}
+                        onKeyDown={handleShowCategories(false)}
+                      >
+                        Tags
+                      </div>
+                    </div>
+                    <div className="labels">
+                      {showCategories
+                        ? categories.map(c => (
+                            <Link
+                              key={c}
+                              className={currentCategory === c ? 'active' : ''}
+                              to={`/blog/category/${c}`}
+                            >
+                              {c}
+                            </Link>
+                          ))
+                        : tags.map(t => (
+                            <Link
+                              key={t}
+                              className={currentTag === t ? 'active' : ''}
+                              to={`/blog/tag/${t}`}
+                            >
+                              {t}
+                            </Link>
+                          ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
---
+++ src/components/postFromUs.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Box, Button, withNormalHelpers } from '@seagreenio/react-bulma'
+
+const NormalBox = withNormalHelpers(Box)
+
+const PostFromUs = () => {
+  const { PostFromUsSVG } = useStaticQuery(
+    graphql`
+      query {
+        PostFromUsSVG: file(relativePath: { eq: "blogs/post-from-us.svg" }) {
+          publicURL
+        }
+      }
+    `
+  )
+
+  return (
+    <NormalBox className="PingCAP-PostFromUs" shadowless>
+      <img src={PostFromUsSVG.publicURL} alt="Post from Us" />
+      <h4 className="title is-4">Never Miss a Post from Us!</h4>
+      <div className="field has-addons">
+        <div className="control">
+          <input className="input" type="text" />
+        </div>
+        <div className="control">
+          <Button color="primary">Subscribe</Button>
+        </div>
+      </div>
+    </NormalBox>
+  )
+}
+
+export default PostFromUs
