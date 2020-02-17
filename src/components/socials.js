1eeec3719223f91c27821414cdb4922dbe8fae5e
Yue Yang <g1enyy0ung@gmail.com>
2020-02-17 19:10:14 +0800

    fix: can't get window in build

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,28 +1,35 @@
+import React, { useEffect, useState } from 'react'
 import { followSocials, shareSocials } from '../data/socials'
 
 import PropTypes from 'prop-types'
-import React from 'react'
 
 const Socials = ({ className, type, title }) => {
-  const data =
-    type === 'follow'
-      ? followSocials
-      : type === 'share'
-      ? shareSocials(window.location.href, title)
-      : followSocials
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    setData(
+      type === 'follow'
+        ? followSocials
+        : type === 'share'
+        ? shareSocials(window.location.href, title)
+        : followSocials
+    )
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
-      {data.map(social => (
-        // eslint-disable-next-line jsx-a11y/anchor-has-content
-        <a
-          key={social.name}
-          className={className ? className + ' ' + social.name : social.name}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={social.href}
-        />
-      ))}
+      {data &&
+        data.map(social => (
+          // eslint-disable-next-line jsx-a11y/anchor-has-content
+          <a
+            key={social.name}
+            className={className ? className + ' ' + social.name : social.name}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={social.href}
+          />
+        ))}
     </>
   )
 }
