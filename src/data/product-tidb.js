09a6ce4e6cc4b144f983020318f04fbce5feaa7c
YiniXu9506 <xuyini@pingcap.com>
2020-06-09 03:02:21 +0800

    feat: add career page and tidb product page

---
+++ src/data/product-tidb.js
@@ -0,0 +1,30 @@
+import financaiTradingSVG from '../../images/products/tidb/financial-trading.svg';
+import ECommerceSVG from '../../images/products/tidb/e-commerce.svg';
+import VideoSVG from '../../images/products/tidb/video.svg';
+import GamingSVg from '../../images/products/tidb/gaming.svg';
+import EducationTechSVG from '../../images/products/tidb/education-tech.svg';
+
+const industryIcons = [
+    {
+        icon: financaiTradingSVG,
+        desc: 'Financial Trading'
+    },
+    {
+        icon: ECommerceSVG,
+        desc: 'E-Commerce'
+    },
+    {
+        icon: VideoSVG,
+        desc: 'Video'
+    },
+    {
+        icon: GamingSVg,
+        desc: 'Gaming'
+    },
+    {
+        icon: EducationTechSVG,
+        desc: 'Education Technology'
+    }
+]
+
+export { industryIcons }
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ import Layout from '../components/layout'
 import { PostFromUsInHome } from '../components/postFromUs'
 import SEO from '../components/seo'
 import { replaceTitle } from '../lib/string'
+import SectionUseCases from '../components/sectionUseCases'
 
 const NormalBox = withNormalHelpers(Box)
 
@@ -240,6 +241,8 @@ const IndexPage = ({ data }) => {
           </div>
         </section>
 
+        <SectionUseCases hasLightBg={false} />
+        
         <section className="section section-get-started">
           <div className="container">
             <h2 className="title is-3 has-text-white">
@@ -260,61 +263,6 @@ const IndexPage = ({ data }) => {
                 Get TiDB
               </Button>
             </Buttons>
-          </div>
-        </section>
-
-        <section className="section section-use-cases">
-          <div className="container">
-            <h2 className="title home-title">Use cases</h2>
-            <div className="columns is-multiline oltp-and-htap">
-              <div className="column is-full-mobile">
-                <Box className="oltp" onClick={onCardClick('/')}>
-                  <div className="inner">
-                    <h3 className="title is-3 is-spaced">O L T P</h3>
-                    <h4 className="title is-4">
-                      Scale-out MySQL for business growth
-                    </h4>
-                    <div className="strikethrough-white" />
-                    <div className="subtitle is-5">
-                      Scalable online transactional processing
-                    </div>
-                    <ul>
-                      <li>
-                        200+ TB production data within a single TiDB cluster
-                      </li>
-                      <li>Trillions of Rows in a single distributed table</li>
-                    </ul>
-                    <Link className="link-with-arrow" to="/">
-                      <ArrowForwardIcon />{' '}
-                      <span>See how modern applications scale</span>
-                    </Link>
-                  </div>
-                </Box>
-              </div>
-              <div className="column is-full-mobile">
-                <Box className="htap" onClick={onCardClick('/')}>
-                  <div className="inner">
-                    <h3 className="title is-3 is-spaced">H T A P</h3>
-                    <h4 className="title is-4">Real-Time Analytics</h4>
-                    <div className="strikethrough-white" />
-                    <div className="subtitle is-5">
-                      Hybrid transactional and analytical processing
-                    </div>
-                    <ul>
-                      <li>Milliseconds response time over trillions of rows</li>
-                      <li>
-                        No wall between transactional data and analytical
-                        reporting
-                      </li>
-                    </ul>
-                    <Link className="link-with-arrow" to="/">
-                      <ArrowForwardIcon />{' '}
-                      <span>See how modern business makes decisions</span>
-                    </Link>
-                  </div>
-                </Box>
-              </div>
-            </div>
           </div>
         </section>
 
