a642e346d460566bb64a4e6db0fdb6162a51dc66
xuechun Lei <xuechunlei922@gmail.com>
2020-06-12 11:49:22 +0800

    feat: add tidb cloud landing page and get trial page

---
+++ src/pages/products/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/layout'
+import React from 'react'
+import SEO from '../../components/seo'
+
+const ProductPage = () => (
+  <Layout>
+    <SEO title="Products" />
+  </Layout>
+)
+
+export default ProductPage
---
+++ src/pages/products/tidb-cloud/index.js
@@ -0,0 +1,336 @@
+import '../../../styles/pages/products/tidbCloud.scss'
+
+import { Link, graphql } from 'gatsby'
+import React, { useEffect, useState } from 'react'
+import {
+  featuresData,
+  logos,
+  reasonsData
+} from '../../../data/products/tidb-cloud'
+
+import { Button } from '@seagreenio/react-bulma'
+import Layout from '../../../components/layout'
+import SEO from '../../../components/seo'
+import axios from 'axios'
+
+const StartTrialButton = () => (
+  <Button
+    as={Link}
+    color="primary"
+    className="start-trial-button"
+    rounded
+    to="/products/tidb-cloud/trial"
+  >
+    Start Free Trial
+  </Button>
+)
+
+const cloudProviders = ['aws', 'googleCloud']
+
+const precision = (num, precision = 12) => {
+  if (num === null || num === undefined) return '-'
+  return +parseFloat(num.toPrecision(precision))
+}
+
+const http = axios.create({
+  baseURL: '/static'
+})
+
+const HourlyNodeUsageInfo = () => {
+  const [cloud, setCould] = useState('aws')
+  const [region, setRegion] = useState('')
+  const [profiles, setProfiles] = useState({
+    aws: null,
+    googleCloud: null
+  })
+
+  useEffect(() => {
+    async function fetchProfiles() {
+      try {
+        const aws = (await http.get('/aws_profiles.json')).data
+        const googleCloud = (await http.get('/gcp_profiles.json')).data
+        setProfiles({ aws, googleCloud })
+        setRegion(aws.regions && aws.regions[0] ? aws.regions[0].name : '')
+      } catch (error) {
+        console.log('Something wrong:' + error)
+        return
+      }
+    }
+
+    fetchProfiles()
+  }, [])
+
+  useEffect(() => {
+    if (profiles[cloud]) {
+      setRegion(
+        profiles[cloud].regions && profiles[cloud].regions[0]
+          ? profiles[cloud].regions[0].name
+          : ''
+      )
+    }
+  }, [cloud, profiles])
+
+  const handleCloudClick = cloud => () => {
+    setCould(cloud)
+  }
+
+  const handleRegionChange = event => {
+    setRegion(event.target.value)
+  }
+
+  const profile = profiles[cloud]
+
+  const ProfileTable = () => {
+    const instances = profile.instances || []
+    const availableProfiles = instances.filter(p =>
+      p.available_regions.map(r => r.region_name).includes(region)
+    )
+
+    const value = v => (v !== null && v !== undefined ? v : '-')
+
+    const TierTableRow = ({ tier }) => {
+      if (!tier) return null
+
+      const { profile_name, available_regions, tidb, tikv } = tier
+      const availablePrice = available_regions.filter(
+        p => p.region_name === region
+      )[0].price
+
+      const isStandard = profile_name.toLowerCase() === 'standard'
+
+      return (
+        <>
+          <tr className={`${isStandard ? 'has-light-background' : ''}`}>
+            <td rowSpan="2" className="tier-td">
+              {profile_name}
+            </td>
+            <td>TiKV</td>
+            <td>{value(tikv.cpu)} vCPU</td>
+            <td>{value(tikv.memory_gi)} Gi</td>
+            <td>
+              {value(tikv.disks[0].disk_gi)} {tikv.disks[0].disk_type}
+            </td>
+            <td>$ {value(availablePrice.tikv)} /hr</td>
+            <td>$ {precision(availablePrice.tikv * 24 * 30)} /month</td>
+          </tr>
+          <tr className={`${isStandard ? 'has-light-background' : ''}`}>
+            <td>TiDB</td>
+            <td>{tidb.cpu} vCPU</td>
+            <td>{tidb.memory_gi} Gi</td>
+            <td>
+              {value(tidb.disks[0].disk_gi)} {tidb.disks[0].disk_type}
+            </td>
+            <td>$ {value(availablePrice.tidb)} /hr</td>
+            <td>$ {precision(availablePrice.tidb * 24 * 30)} /month</td>
+          </tr>
+        </>
+      )
+    }
+
+    return (
+      <div className="table-container">
+        <table className="table is-bordered comparison-table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Tier</th>
+              <th>Node</th>
+              <th>CPU</th>
+              <th>Memory</th>
+              <th>Storage</th>
+              <th>Hourly Usage Per Node</th>
+              <th>Monthly Usage Per Node</th>
+            </tr>
+          </thead>
+          <tbody>
+            {availableProfiles.map(p => (
+              <TierTableRow key={p.profile_name} tier={p} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  return (
+    <div className="tidb-cloud-hourly-usage">
+      <div className="cloud-providers">
+        {cloudProviders.map(c => (
+          <Button
+            key={c}
+            className="cloud-provider-button"
+            active={cloud === c}
+            onClick={handleCloudClick(c)}
+          >
+            <img
+              className="logo"
+              src={logos[c]}
+              alt={`${c.toUpperCase()} Logo`}
+            />
+          </Button>
+        ))}
+      </div>
+
+      <div className="regions">
+        <span className="desc">Select Region</span>
+        <div className="select">
+          {profile && profile.regions && (
+            <select value={region} onChange={handleRegionChange}>
+              {profile.regions.map(r => (
+                <option key={r.name} value={r.name}>
+                  {r.name}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
+      </div>
+
+      {profile && <ProfileTable />}
+    </div>
+  )
+}
+
+const TiDBCloudPage = ({ data }) => {
+  const { TiDBCloudLogoPNG, BannerSVG } = data
+
+  return (
+    <Layout>
+      <SEO title="TiDB Cloud" />
+      <main className="PingCAP-TiDBCloud">
+        <section className="hero is-medium">
+          <div className="hero-body">
+            <div className="container">
+              <div className="titles-and-entry">
+                <h1>
+                  <img
+                    src={TiDBCloudLogoPNG.publicURL}
+                    className="logo"
+                    alt="TiDB Cloud"
+                  />
+                </h1>
+                <h2 className="title is-4">Fully-managed TiDB Service</h2>
+                <p className="is-7 desc">
+                  Bring everything great about TiDB to your cloud. TiDB Cloud
+                  lets you focus on your applications, not the complexities of
+                  your database.
+                </p>
+                <StartTrialButton />
+                <p className="is-7 desc">
+                  Already have an account?{' '}
+                  <a
+                    className="link-with-underline"
+                    href="https://trial.tidbcloud.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Sign in
+                  </a>
+                </p>
+              </div>
+              <div className="image-wrapper">
+                <img
+                  src={BannerSVG.publicURL}
+                  className="banner"
+                  alt="TiDB Cloud"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="section section-why-tidb-cloud has-light-background">
+          <div className="container">
+            <h2 className="title is-4">Why TiDB Cloud</h2>
+            <div className="reasons">
+              {reasonsData.map(d => (
+                <div key={d.name} className="reason">
+                  <div className="placeholder-wrapper">
+                    <img
+                      className="placeholder"
+                      src={d.placeholder}
+                      alt={d.name}
+                    />
+                  </div>
+                  <div className="intro">
+                    <h3 className="title is-5 is-spaced">{d.name}</h3>
+                    <p className="is-7 desc">{d.desc}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className="section section-features">
+          <div className="container">
+            <h2 className="title is-4">Key Features</h2>
+            <div className="features">
+              {featuresData.map(d => (
+                <div
+                  key={d.name}
+                  className={`feature${d.reverse ? ' reverse' : ''}`}
+                >
+                  <div className="placeholder-wrapper">
+                    <img
+                      className="placeholder"
+                      src={d.placeholder}
+                      alt={d.name}
+                    />
+                  </div>
+                  <div className="divider" />
+                  <div className="intro">
+                    <h3 className="title is-5 is-spaced">{d.name}</h3>
+                    <p className="is-7 desc">{d.desc}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className="section section-pricing has-light-background">
+          <div className="container">
+            <h2 className="title is-4">Pricing</h2>
+            <div className="field">
+              <h3 className="title is-5">Hourly Node Usage</h3>
+              <p className="is-7 desc">
+                TiDB Cloud provides different cluster tiers. Detailed pricing
+                for TiDB/TiKV instances in different tiers are as follows:
+              </p>
+              <HourlyNodeUsageInfo />
+              <p className="is-7 desc">
+                Data backup and data transfer need to be charged separately. See{' '}
+                <Link className="link-with-underline" to="/">
+                  Billing
+                </Link>{' '}
+                for more details.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className="section section-get-started">
+          <div className="container">
+            <h2 className="title is-4">Get Started with TiDB Cloud</h2>
+            <StartTrialButton />
+          </div>
+        </section>
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    TiDBCloudLogoPNG: file(
+      relativePath: { eq: "products/tidb-cloud/tidb-cloud-logo.png" }
+    ) {
+      publicURL
+    }
+    BannerSVG: file(relativePath: { eq: "products/tidb-cloud/banner.svg" }) {
+      publicURL
+    }
+  }
+`
+
+export default TiDBCloudPage
