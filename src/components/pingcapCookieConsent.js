8eb07da5c578b69c7a9065d2e989076888a99ee9
YiniXu9506 <xuyini@pingcap.com>
2020-06-22 10:09:08 +0800

    chore: update cookie consent layout

--- src/components/pingcapCookieConsent.js
+++ src/components/pingcapCookieConsent.js
@@ -2,38 +2,38 @@ import '../styles/components/pingcapCookieConsent.scss'
 
 import CookieConsent from 'react-cookie-consent'
 import React from 'react'
+import { Link } from 'gatsby'
 
 const PingCAPCookieConsent = () => {
-  const buttonStyle = {
-    color: '#6e77a3',
-    fontSize: '13px',
-    background: '#fff'
-  }
-
-  const cookieBannerStyle = {
-    background: '#6e77a3'
-  }
 
   return (
     <section className="PingCAP-Cookie-Consent">
-      <div className="container">
         <CookieConsent
           location="bottom"
           buttonText="Accept"
-          cookieName="gatsby-gdpr-google-analytics"
           enableDeclineButton
           declineButtonText="Decline"
-          style={cookieBannerStyle}
-          buttonStyle={buttonStyle}
+          contentClasses="content"
+          cookieName="gatsby-gdpr-google-analytics"
+          buttonClasses="accept-button"
           declineButtonClasses="decline-button"
           expires={150}
         >
-          This website uses cookies to enhance the user experience.{' '}
-          <span style={{ fontSize: '10px' }}>
-            This bit of text is smaller :O
-          </span>
+          <p>
+            This website stores cookies on your computer. These cookies are used
+            to collect information about how you interact with our website and
+            allow us to remember you. We use this information in order to
+            improve and customize your browsing experience and for analytics and
+            metrics about our visitors both on this website and other media. To
+            find out more about the cookies we use, see our{' '}
+            <Link to="/cookie-policy">Cookie Policy</Link>.
+          </p>
+          <p>
+            If you decline, your information won’t be tracked when you visit
+            this website. A single cookie will be used in your browser to
+            remember your preference not to be tracked.
+          </p>
         </CookieConsent>
-      </div>
     </section>
   )
 }
