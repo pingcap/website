67b3752839505caab11b808332d836070a9659b6
YiniXu9506 <xuyini@pingcap.com>
2020-06-23 21:42:57 +0800

    chore: update replace path function

--- create-pages/utils.js
+++ create-pages/utils.js
@@ -1,7 +1,3 @@
 exports.replaceTitle = function(title) {
-  if (title.endsWith('.md')) {
-    return title.replace('.md', '')
-  } else {
-    return title.replace(/[^\w-]/g, '-')
-  }
+  return title.replace('.md', '')
 }
