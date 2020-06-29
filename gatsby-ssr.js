4b626d4a8f1f857d55e94c61302a3d5224549bf9
Haichuan Zhang <me@std4453.com>
2020-06-29 13:29:10 +0800

    Fix React not in scope error

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,6 +6,9 @@
 
 // You can delete this file if you're not using it
 
+import React from 'react'
+import { IntlProvider } from 'react-intl'
+
 export const wrapPageElement = ({ element, props }) => {
   const { pageContext } = props
 
