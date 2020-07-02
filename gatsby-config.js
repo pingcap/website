53e381ac3880835ae49fd95ab977d041b153338f
YiniXu9506 <xuyini@pingcap.com>
2020-07-02 20:22:39 +0800

    chore: remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,9 +95,7 @@ module.exports = {
         icon: 'images/pingcap-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: `gatsby-plugin-sass`,
       options: {
