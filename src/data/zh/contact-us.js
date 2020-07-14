4b9b78e6b3c4df6f05edfdf3169dae8f4f6ec04f
wanyoulc <37950176+wanyoulc@users.noreply.github.com>
2020-07-14 11:40:02 +0800

    About us page (#89)
    * feat: finish about-us
    
    * chore: delete console log in scroll top button
    
    * update: refactor ui
    
    * chore: modified contact-us data
    
    * update: responsive
    
    Co-authored-by: Yini Xu <34967660+YiniXu9506@users.noreply.github.com>

---
+++ src/data/zh/contact-us.js
@@ -0,0 +1,38 @@
+const offices = [
+  {
+    name: '北京 Office',
+    address: '北京市海淀区西小口路66号东升科技园C-1楼2层',
+  },
+  {
+    name: '上海 Office',
+    address: '上海市黄浦区大沽路288号大沽庭映像5号楼2楼B/C',
+  },
+  {
+    name: '杭州 Office',
+    address: '杭州市文一西路588号“中节能·西溪首座”5号楼A2-1 7层722室',
+  },
+  {
+    name: '成都 Office',
+    address: '成都市天府大道中段666号希顿国际B座2806',
+  },
+  {
+    name: '深圳 Office',
+    address: '深圳市宝安区卓越前海时代广场A座2903',
+  },
+  {
+    name: '广州 Office',
+    address: '广州市海珠区暄悦东街23号保利中悦广场3702',
+  },
+  {
+    name: '硅谷 Office',
+    address: '2955 Campus Drive # 100, San Mateo, CA 94403',
+  },
+]
+
+const contacts = [
+  { way: '电话', address: '+86 010-53326356' },
+  { way: '技术及商务咨询', address: 'info@pingcap.com' },
+  { way: '媒体合作', address: 'pr@pingcap.com' },
+]
+
+export { offices, contacts }
---
+++ src/data/zh/map.js
@@ -0,0 +1,46 @@
+const mapData = [
+  {
+    name: '北京office',
+    dotOffset: { right: 213.56, top: 110.32 },
+    buttonOffset: { left: 115, top: -115 },
+    line: { width: 160, angel: 315 },
+  },
+  {
+    name: '上海office',
+    dotOffset: { right: 213.56, top: 133.96 },
+    buttonOffset: { left: 120, top: -10 },
+    line: { width: 120, angel: 0 },
+  },
+  {
+    name: '杭州office',
+    dotOffset: { right: 224.8, top: 145.78 },
+    buttonOffset: { left: 120, top: 100 },
+    line: { width: 170, angel: 45 },
+  },
+  {
+    name: '成都office',
+    dotOffset: { right: 314.72, top: 133.96 },
+    buttonOffset: { left: -130, top: 53 },
+    line: { width: 90, angel: 145 },
+  },
+  {
+    name: '深圳office',
+    dotOffset: { right: 236.04, top: 165.48 },
+    buttonOffset: { left: 90, top: 140 },
+    line: { width: 170, angel: 60 },
+  },
+  {
+    name: '广州office',
+    dotOffset: { right: 252.9, top: 161.54 },
+    buttonOffset: { left: -100, top: 100 },
+    line: { width: 120, angel: 125 },
+  },
+  {
+    name: '硅谷office',
+    dotOffset: { left: 179.84, top: 145.78 },
+    buttonOffset: { left: -131, top: 75 },
+    line: { width: 100, angel: 125 },
+  },
+]
+
+export { mapData }
