357234e4bce1428fd4ccc903401835115421e86e
Haichuan Zhang <me@std4453.com>
2020-07-17 18:17:09 +0800

    Add promotion banner

--- src/data/navbar.js
+++ src/data/navbar.js
@@ -1,38 +1,55 @@
+import React from 'react'
+
 const navbarItems = [
   {
     name: 'Product',
     dropdown: [
       {
         name: 'TiDB',
-        href: '/products/tidb'
+        href: '/products/tidb',
       },
       {
         name: 'TiDB Cloud',
-        href: '/products/tidbcloud'
-      }
-    ]
+        href: '/products/tidbcloud',
+      },
+    ],
   },
   {
     name: 'Case Studies',
-    href: '/case-studies'
+    href: '/case-studies',
   },
   {
     name: 'Docs',
     href: 'https://docs.pingcap.com/tidb/v4.0',
-    outbound: true
+    outbound: true,
   },
   {
     name: 'Blog',
-    href: '/blog'
+    href: '/blog',
   },
   {
     name: 'Community',
-    href: '/community'
+    href: '/community',
   },
   {
     name: 'Contact Us',
-    href: '/contact-us'
-  }
+    href: '/contact-us',
+  },
 ]
 
-export { navbarItems }
+// to hide promotion, uncomment:
+// const promotionText = null
+const promotionText = (
+  <>
+    TiDB Cloud (beta) is open for free trial now. Apply&nbsp;
+    <a
+      href="https://pingcap.com/products/tidbcloud"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Here
+    </a>
+  </>
+)
+
+export { navbarItems, promotionText }
