60b171a92eec97ab0301a6a2b477b37dc5c9c4fc
wanyoulc <15694528183@163.com>
2020-07-21 08:12:44 +0800

    fix: fixed build error since zh-logos imported

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -69,7 +69,6 @@ const CaseStudies = ({ data }) => {
 
   return (
     <Layout>
-      <ColumnsForDebugging></ColumnsForDebugging>
       <SEO
         title="TiDB Case Studies"
         description="As a distributed, NewSQL, Hybrid Transactional/Analytical Processing database, TiDB is trusted and verified by web-scale application leaders."
