af6f0222438cf743d255f297ce1e1cf7ef737e69
Haichuan Zhang <me@std4453.com>
2020-07-27 14:10:57 +0800

    Add pagination

--- src/pages/zh/user-message-2019.js
+++ src/pages/zh/user-message-2019.js
@@ -92,35 +92,53 @@ const UserMessage2019 = () => {
     new Swiper('.swiper-pages', {
       direction: 'vertical',
       loop: false,
-      // pagination: {
-      //     el: '.swiper-custom-pagination',
-      //     clickable: true,
-      //     bulletClass: 'bullet',
-      //     bulletActiveClass: 'active',
-      //     renderBullet: () => `<span className="bullet"></span>`,
-      // },
-      mousewheel: {
-        releaseOnEdges: true,
-      },
-    })
-
-    new Swiper('.swiper-1', {
-      loop: false,
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 56,
-      navigation: {
-        nextEl: '.swiper-1-next',
-        prevEl: '.swiper-1-prev',
-      },
       pagination: {
-        el: '.swiper-1-pagination',
+        el: '.pages-pagination',
         clickable: true,
         bulletClass: 'bullet',
         bulletActiveClass: 'active',
+        renderBullet: function (index, className) {
+          return `<span class="${className} page-${index + 1}-bullet"></span>`
+        },
+      },
+      mousewheel: {
+        releaseOnEdges: true,
+      },
+
+      watchSlidesProgress: true,
+      virtualTranslate: true,
+      on: {
+        setTransition: function (transitionSpeed) {
+          for (const slide of Array.from(this.slides)) {
+            slide.style.transition = `transform cubic-bezier(0.25, 0.46, 0.45, 0.94) ${transitionSpeed}ms`
+            slide.style.setProperty(
+              '--transition-speed',
+              `${transitionSpeed}ms`
+            )
+          }
+        },
+        setTranslate: function () {
+          for (const slide of Array.from(this.slides)) {
+            const translate =
+              -slide.swiperSlideOffset -
+              slide.progress * (slide.progress > 0 ? 0.3 : 1) * this.height
+            slide.style.transform = `translate3d(0, ${translate}px, 0)`
+            slide.style.setProperty(
+              '--backdrop-opacity',
+              Math.max(0, Math.min(slide.progress, 1))
+            )
+          }
+        },
+        paginationUpdate: function () {
+          if (this.activeIndex === 0 || this.activeIndex === 4) {
+            this.pagination.el.classList.add('hidden')
+          } else {
+            this.pagination.el.classList.remove('hidden')
+          }
+        },
       },
     })
-  }, [])
+  })
 
   return (
     <Layout>
@@ -130,8 +148,25 @@ const UserMessage2019 = () => {
       />
       <article className="PingCAP-UserMessage2019">
         <div className="page swiper-container swiper-pages">
+          <div className="pages-pagination" />
           <div className="swiper-wrapper">
-            <div className="swiper-slide page page-1"></div>
+            <div className="swiper-slide page page-1">
+              <div className="title">TiDB 非官方使用指南</div>
+              <div className="content">
+                我们邀请了 48 位用户代表
+                <br />
+                汇成了这份“非官方使用指南”
+                <br />
+                这些实操中闯过的关和踩过的坑
+                <br />
+                创造了 TiDB 的更多可能
+                <br />
+                <br />
+                一切伟大，都源于开始
+                <br />
+                2020 到来了，感谢大家
+              </div>
+            </div>
             <SliderPage
               messages={data.section1}
               pageClass="page-2"
@@ -147,9 +182,7 @@ const UserMessage2019 = () => {
               pageClass="page-4"
               title="面对大家的期待，我们还在努力完善，也希望能得到更多的反馈"
             />
-            <div className="swiper-slide page page-3"></div>
-            <div className="swiper-slide page page-4"></div>
-            <div className="swiper-slide page page-5"></div>
+            <div className="swiper-slide page page-5" />
           </div>
         </div>
       </article>
