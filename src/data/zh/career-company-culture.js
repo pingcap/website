bbc2d794a4011562a4643b7d2a0ec9be802c48a5
wanyoulc <15694528183@163.com>
2020-07-28 09:45:09 +0800

    update: updated zh document

---
+++ src/data/zh/career-company-culture.js
@@ -0,0 +1,45 @@
+import hackerCultureSVG from '../../../images/careers/hacker-culture.svg'
+import competitiveSVG from '../../../images/careers/competitive.svg'
+import flexibleSVG from '../../../images/careers/flexible.svg'
+import collaborativeSVg from '../../../images/careers/collaborative.svg'
+import tailoredGrowthSVG from '../../../images/careers/tailored-growth.svg'
+import swagPackageSVG from '../../../images/careers/swag-package.svg'
+
+const companyCulture = [
+  {
+    icon: hackerCultureSVG,
+    title: '持续的学习成长',
+    desc:
+      'One-on-One Mentoring 培养方式 + Learning Bonus 助力每一位爱学习的 PingCAPer 不断成长。',
+  },
+  {
+    icon: competitiveSVG,
+    title: 'High-Tech 的技术交流',
+    desc: '内容丰富的Infra Meetup 和 Technical Meetings 技术实力 upup！',
+  },
+  {
+    icon: tailoredGrowthSVG,
+    title: '技术人的DNA',
+    desc:
+      '我们有各种知名开源项目作者 还有代表未来的并对技术永葆热情和追求的你！',
+  },
+  {
+    icon: collaborativeSVg,
+    title: '灵活的办公地点',
+    desc:
+      '北京、上海、广州、杭州、硅谷、Remote…… 在哪里不重要，重要的是 Get Things Done',
+  },
+  {
+    icon: flexibleSVG,
+    title: '弹性的工作时间',
+    desc: '不用打卡，不需要早 9 晚 6 和你的生物钟来场大和解吧！',
+  },
+  {
+    icon: swagPackageSVG,
+    title: '丰富的周边礼品',
+    desc:
+      '充电宝、马克杯、背包、T-shirt、雨伞…… 是时候考虑一下什么时候上线 PingCAP 周边官方旗舰店了。',
+  },
+]
+
+export default companyCulture
--- src/pages/zh/careers.js
+++ src/pages/zh/careers.js
@@ -5,12 +5,12 @@ import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 import AroundParticles from '../../components/particles'
 import { graphql, Link } from 'gatsby'
-import { workEnv } from '../../data/career-work-env'
+import companyCulture from '../../data/zh/career-company-culture'
 import { Button } from '@seagreenio/react-bulma'
 import pingcaperData from '../../data/zh/pingcaper'
 
 const Careers = ({ data }) => {
-  const { careerHeroSVG, rightArrowSVG } = data
+  const { careerHeroSVG } = data
   return (
     <Layout>
       <SEO
@@ -22,21 +22,18 @@ const Careers = ({ data }) => {
           <div className="hero-body">
             <div className="container has-text-centered">
               <img src={careerHeroSVG.publicURL} alt="Hyper Growth Hero" />
-              <h1 className="title">
-                Join the team that is building the database of the future
-              </h1>
+              <h1 className="title">加入我们</h1>
               <div className="subtitle-under-main-title">
-                Our mission is to build a database of the future, made by
-                developers, for developers. It’s an ambitious mission, and we
-                can’t do it without curious, self-driven, and capable
-                people...like you!
+                我们认为优秀的人或多或少有以下共同特质：Quick Learner, Earnest
+                Curiosity, Faith in Open Source, Self-driven, Get Things Done.
+                如果你符合以上特质，点击下方获取新的工作机会吧！
               </div>
               <Button
                 as={Link}
                 to="/careers#see-more-positions"
                 className="button is-primary is-rounded"
               >
-                See Open Positions
+                查看职位
               </Button>
             </div>
           </div>
@@ -44,18 +41,15 @@ const Careers = ({ data }) => {
 
         <section className="section working-env-section">
           <div className="container">
-            <h2 className="title">Working at PingCAP</h2>
+            <h2 className="title">体验纯正的工程师文化</h2>
             <div className="subtitle-under-main-title">
-              At PingCAP, you find yourself working with the most talented, and
-              motivated people in the industry. Apart from inspirations and
-              productivity, we do our best to provide you with a comfortable and
-              free working environment.
+              与行业中最有才华、最积极的人一起工作，除了灵感和生产力，还有最纯正的工程师文化！
             </div>
             <div className="work-env-container">
-              {workEnv.map((w) => (
-                <div className="env-item" key={w.desc}>
+              {companyCulture.map((w) => (
+                <div className="env-item" key={w.title}>
                   <img src={w.icon} alt={w.desc} />
-                  <p className="env-title">{w.desc}</p>
+                  <p className="env-title">{w.title}</p>
                   {/* mock data */}
                   <p className="env-desc">{w.desc}</p>
                 </div>
