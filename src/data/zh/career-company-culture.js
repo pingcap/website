3b6772f7e0b9b868317636b7a2c54559a3c5a06f
wanyoulc <37950176+wanyoulc@users.noreply.github.com>
2020-07-29 10:31:18 +0800

    Zh career index (#122)
    * update: updated zh career
    
    * update: updated zh career
    
    * fix: fixed ui details
    
    * update: updated mobile ui
    
    * fix: fixed ui problem, which comes from using sass variable in media query
    
    * update: updated zh document
    
    * fix: fixed ui, put on the newest icons
    
    * fix: fixed some problems by comments
    
    * fix: fixed font size of title in career index
    
    Co-authored-by: Yini Xu <34967660+YiniXu9506@users.noreply.github.com>

---
+++ src/data/zh/career-company-culture.js
@@ -0,0 +1,45 @@
+import growthSVG from '../../../images/zh/career/work-env/growth.svg'
+import techCommunication from '../../../images/zh/career/work-env/tech-communication.svg'
+import flexibleSVG from '../../../images/zh/career/work-env/flexible.svg'
+import officeSVG from '../../../images/zh/career/work-env/office.svg'
+import dnaSVG from '../../../images/zh/career/work-env/DNA.svg'
+import swagPackageSVG from '../../../images/zh/career/work-env/swag-package.svg'
+
+const companyCulture = [
+  {
+    icon: growthSVG,
+    title: '持续的学习成长',
+    desc:
+      'One-on-One Mentoring 培养方式 + Learning Bonus 助力每一位爱学习的 PingCAPer 不断成长。',
+  },
+  {
+    icon: techCommunication,
+    title: 'High-Tech 的技术交流',
+    desc: '内容丰富的Infra Meetup 和 Technical Meetings 技术实力 upup！',
+  },
+  {
+    icon: dnaSVG,
+    title: '技术人的DNA',
+    desc:
+      '我们有各种知名开源项目作者 还有代表未来的并对技术永葆热情和追求的你！',
+  },
+  {
+    icon: officeSVG,
+    title: '灵活的办公地点',
+    desc:
+      '北京、上海、广州、杭州、硅谷、Remote…… 在哪里不重要，重要的是 Get Things Done',
+  },
+  {
+    icon: flexibleSVG,
+    title: '弹性的工作时间',
+    desc: '不用打卡，不需要早 9 晚 6 和你的生物钟来场大和解吧！',
+  },
+  {
+    icon: swagPackageSVG,
+    title: '丰富的周边礼品',
+    desc:
+      '充电宝、马克杯、背包、T-shirt、雨伞…… 是时候考虑一下什么时候上线 PingCAP 周边官方旗舰店了。',
+  },
+]
+
+export default companyCulture
