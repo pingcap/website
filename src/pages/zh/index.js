4bc90255b0a5a4b8f36dabd27cf0afa2c5b493b4
Yue Yang <g1enyy0ung@gmail.com>
2020-08-07 18:19:50 +0800

    chore: add campus 2021 banner

--- src/pages/zh/index.js
+++ src/pages/zh/index.js
@@ -13,6 +13,7 @@ import { replaceTitle } from '../../lib/string'
 import LinkWithArrow from '../../components/linkWithArrow'
 import StartTiDBRibbon from '../../components/startTiDBRibbon'
 
+import campus2021 from '../../../images/home/banners/zh/campus-2021.jpg'
 import askTug from '../../../images/home/banners/zh/home-asktug-carousel.jpg'
 import university from '../../../images/home/banners/zh/university-carousel-img.png'
 import userMessage from '../../../images/home/banners/zh/user-message-2019-banner.png'
@@ -55,6 +56,7 @@ const caseLogos = [
 ]
 
 const banners = [
+  { img: campus2021, url: '/zh/careers/Campus' },
   { img: askTug, url: 'https://asktug.com/t/topic/33760/2' },
   { img: university, url: 'https://university.pingcap.com/' },
   { img: userMessage, url: '/zh/user-message-2019/' },
