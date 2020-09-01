705a052977c6fd8c80d65df082ff925c81f58abb
YiniXu9506 <xuyini@pingcap.com>
2020-09-01 18:05:24 +0800

    fix: check blogPath validation

--- src/pages/blog-cn.js
+++ src/pages/blog-cn.js
@@ -5,15 +5,17 @@ import { navigate } from 'gatsby'
 const BlogCN = () => {
   const location = useLocation()
   const blogTag = location.hash.slice(1)
-  const blogPathName = location.pathname.split('/')
+  const blogPathName = location.pathname.split('/')[2]
 
   const RedirectBlogCNPath = () => {
     // redirect /blog-cn/#tag
     if (blogTag) {
       navigate(`/zh/blog/tag/${blogTag}`)
-    } else {
+    } else if (blogPathName) {
       // redirect /blog-cn/BLOG_PATH
-      navigate(`/zh/blog/${blogPathName[2]}`)
+      navigate(`/zh/blog/${blogPathName}`)
+    } else {
+      navigate('/zh/blog')
     }
     return null
   }
