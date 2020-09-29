9f4ea4d657327b532453c6edcc41c8c38bd28e2b
YiniXu9506 <xuyini@pingcap.com>
2020-09-29 15:57:38 +0800

    chore: sync positions using Lever API

--- src/components/positions.js
+++ src/components/positions.js
@@ -1,38 +1,43 @@
 import '../styles/components/positions.scss'
 
-import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import React, { useState, useEffect } from 'react'
 import Link from './IntlLink'
+import axios from 'axios'
 
 const Positions = () => {
-  let { positions } = useStaticQuery(graphql`
-    query {
-      positions: allPositionsJson {
-        edges {
-          node {
-            link
-            location
-            title
-          }
-        }
-      }
-    }
-  `)
+  const [positions, setPositions] = useState([])
 
-  positions = positions.edges.map((edge) => edge.node)
+  async function fetchPositions() {
+    try {
+      const positionRes = (
+        await axios.get('https://api.lever.co/v0/postings/pingcap?mode=json')
+      ).data
+      setPositions(positionRes)
+      console.log('positions', positionRes)
+    } catch (e) {
+      console.log('Failed to fetch positions')
+      return
+    }
+  }
+
+  useEffect(() => {
+    fetchPositions()
+  }, [])
 
   return (
     <div className="PingCAP-Positions columns is-multiline">
       {positions.map((p) => (
         <Link
-          to={p.link}
-          key={p.link}
+          to={p.hostedUrl}
+          key={p.hostedUrl}
           className="position column is-4"
           type="outBoundLink"
         >
           <div className="position-wrapper">
-            <div className="position-title">{p.title}</div>
-            <div className="location">{p.location}</div>
+            <div className="position-title">{p.text}</div>
+            <div className="location">
+              {p.categories.location}/{p.categories.commitment}
+            </div>
           </div>
         </Link>
       ))}
