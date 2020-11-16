4858ff9b3aeff5bf2149214bbfbb5802add7ad5c
昌维 <changwei@pingcap.com>
2020-11-16 15:09:51 +0800

    Add page: security and trust center (#290)
    * create redirect: from /download/community to /download
    
    * fix a bug about redirect rule: /download/community/
    
    * remove page: /download/community
    
    * add images resource: security-and-trust-center
    
    * add component: banner
    
    * add page: security and trust center, including banner, summary and Key Security Features
    
    * optimize style on mobile
    
    * add component: get-started-with-TiDB-cloud
    
    * add page: security-and-trust-center
    
    * fix a bug about Layout of the components' importing path
    
    * update route path: from /security-and-trust-center to /trust
    
    * add entry "Trust Center" in the footer, under Resources
    
    * update features copywriting
    
    * update image, reduce file size
    
    * add page padding
    
    * update features item style, use bulma columns style
    
    * update SecurityAndTrustCenterCompliance-more copywriting and style
    
    * Update page "trust" style
    
    * update component "GetStartedWithTiDBCloud"
    
    * update features style, use list-style: inside insteda of graphic character disc
    
    * remove redundant classname constant
    
    * extract features data from pages/trust.js to data/trust.js

---
+++ src/pages/trust.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+
+import '../styles/pages/security-and-trust-center.sass'
+import Banner from '../components/banner'
+import SEO from '../components/seo'
+import Layout from '../components/layout'
+import GetStartedWithTiDBCloud from '../components/get-started-with-TiDB-cloud'
+
+import trustData from '../data/trust'
+
+const Trust = React.memo(({ data }) => {
+  const { BannerSVG, AICPA_SOC_PNG } = data
+  const className = `SecurityAndTrustCenter`
+  return (
+    <Layout>
+      <SEO
+        title="About PingCAP"
+        description="Story about PingCAP, the team behind TiDB"
+      />
+      <article className="PingCAP-SecurityAndTrustCenter">
+        <Banner backgroundImage={BannerSVG.publicURL}>
+          Security and Trust Center
+        </Banner>
+        <div className={`${className} container`}>
+          <div className="SecurityAndTrustCenterSummary">
+            At PingCAP we value security and trust more than anything because we
+            know our customers entrust us with their most important asset - the
+            data itself. We take this responsibility seriously and are always
+            dedicated to protecting the Security, Availability, and
+            Confidentiality of our customers’ data. We have incorporated
+            security into all aspects of our offering (TiDB Cloud) and
+            operations.
+          </div>
+          <SecurityAndTrustCenterKeySecurityFeatures
+            title={'Key Security Features'}
+            list={trustData.featuresList}
+          >
+            TiDB Cloud is designed with strict security measures in all aspects
+            that are aligned with the market, so that you can focus on your
+            data, not protecting it.
+          </SecurityAndTrustCenterKeySecurityFeatures>
+        </div>
+        <SecurityAndTrustCenterCompliance reportIcon={AICPA_SOC_PNG} />
+        <GetStartedWithTiDBCloud />
+      </article>
+    </Layout>
+  )
+})
+
+const SecurityAndTrustCenterKeySecurityFeatures = React.memo(
+  ({ title, children, list }) => {
+    const className = `SecurityAndTrustCenterKeySecurityFeatures`
+    return (
+      <div className={className}>
+        <div className={`${className}-title`}>{title}</div>
+        <div className={`${className}-body`}>{children}</div>
+        <div className={`${className}-list columns`}>
+          {list.map((item) => (
+            <SecurityAndTrustCenterKeySecurityFeaturesItem data={item} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+)
+
+const SecurityAndTrustCenterKeySecurityFeaturesItem = React.memo(({ data }) => {
+  const { icon, title, list } = data
+
+  const className = `SecurityAndTrustCenterKeySecurityFeaturesItem`
+  return (
+    <div className={`${className} column`}>
+      <div className={`${className}-icon`}>
+        <img src={icon} alt={title} />
+      </div>
+      <div className={`${className}-title`}>{title}</div>
+      <div className={`${className}-line`} />
+      <div className={`${className}-list`}>
+        {Array.isArray(list) ? (
+          <ul>
+            {list.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        ) : (
+          list
+        )}
+      </div>
+    </div>
+  )
+})
+
+const SecurityAndTrustCenterCompliance = React.memo(({ reportIcon }) => {
+  const className = `SecurityAndTrustCenterCompliance`
+  const classNameReport = `${className}-report`
+  const classNameReportLeft = `${classNameReport}-left`
+  const classNameReportRight = `${classNameReport}-right`
+  const classNameReportRightHeader = `${classNameReportRight}-header`
+  return (
+    <div className={className}>
+      <div className="container">
+        <div className={`${className}-title`}>Compliance</div>
+        <div className={`${className}-summary`}>
+          We are committed to providing enterprise-grade security and privacy.
+          This is not simply demonstrated in technology. We undergo third-party
+          auditing to ensure our services and operations adhere to the
+          compliance requirements of your organization. TiDB Cloud operates in
+          accordance with the following compliance requirements:
+        </div>
+        <Link
+          to={
+            '/blog/pingcap-successfully-completes-soc-2-type-1-examination-for-tidb-cloud'
+          }
+        >
+          <div className={classNameReport}>
+            <div className={classNameReportLeft}>
+              <img src={reportIcon.publicURL} alt="SOC 2 Type 1 Report" />
+            </div>
+            <div className={classNameReportRight}>
+              <div className={classNameReportRightHeader}>
+                <div className={`${classNameReportRightHeader}-title`}>
+                  SOC 2 Type 1 Report
+                </div>
+                <div className={`${classNameReportRightHeader}-more`}>
+                  <Link
+                    to={
+                      '/blog/pingcap-successfully-completes-soc-2-type-1-examination-for-tidb-cloud'
+                    }
+                  >
+                    → Learn More
+                  </Link>
+                </div>
+              </div>
+              <div className={`${classNameReportRight}-description`}>
+                The SOC 2 Type I audit is an independent audit designed and
+                conducted by Schellman & Company, LLC based on the security &
+                privacy related control and operations of the TiDB Cloud service
+                offering and the standards formulated by the American Institute
+                of Certified Public Accountants (AICPA).
+              </div>
+            </div>
+          </div>
+        </Link>
+        <div className={`${className}-more`}>
+          More compliance audits are in progress...
+        </div>
+      </div>
+    </div>
+  )
+})
+
+export const query = graphql`
+  query {
+    BannerSVG: file(
+      relativePath: { eq: "security-and-trust-center/banner.jpg" }
+    ) {
+      publicURL
+    }
+    AICPA_SOC_PNG: file(
+      relativePath: { eq: "security-and-trust-center/AICPA-SOC.gif" }
+    ) {
+      publicURL
+    }
+  }
+`
+
+export default Trust
