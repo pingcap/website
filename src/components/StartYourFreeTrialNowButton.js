68d30f5162c457abce45c129dbf840701c73d5f3
昌维 <867597730@qq.com>
2020-11-19 11:37:00 +0800

    extract component StartYourFreeTrialNowButton from component GetStartedWithTiDBCloud, because it will be used by other page

---
+++ src/components/StartYourFreeTrialNowButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { Button } from '@seagreenio/react-bulma'
+
+import '../styles/components/StartYourFreeTrialNowButton.sass'
+
+const StartYourFreeTrialNowButton = () => {
+  const className = `StartYourFreeTrialNowButton`
+  return (
+    <Button
+      as={Link}
+      color="primary"
+      className={className}
+      rounded
+      to="/products/tidbcloud/trial"
+    >
+      Start Your Free Trial Now
+    </Button>
+  )
+}
+
+export default StartYourFreeTrialNowButton
