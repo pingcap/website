97a93a03b4f7ded9a93608f4e0cb2b88b7ba8c9e
昌维 <867597730@qq.com>
2020-11-24 11:16:36 +0800

    reuse button component on component StartYourFreeTrialNowButton

--- src/components/startYourFreeTrialNowButton.js
+++ src/components/startYourFreeTrialNowButton.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Button } from '@seagreenio/react-bulma'
 
 import '../styles/components/startYourFreeTrialNowButton.scss'
+import PrimaryButton from './primaryButton'
 
 const StartYourFreeTrialNowButton = () => {
-  const className = `StartYourFreeTrialNowButton`
   return (
-    <Button
-      as={Link}
-      color="primary"
-      className={className}
-      rounded
-      to="/products/tidbcloud/trial"
-    >
+    <PrimaryButton as={Link} to="/products/tidbcloud/trial">
       Start Your Free Trial Now
-    </Button>
+    </PrimaryButton>
   )
 }
 
