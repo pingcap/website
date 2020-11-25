e163741e8256066062ee40f124055fd72a2c7da9
昌维 <867597730@qq.com>
2020-11-25 12:23:31 +0800

    remove unused style file: startYourFreeTrialNowButton.scss

--- src/components/startYourFreeTrialNowButton.js
+++ src/components/startYourFreeTrialNowButton.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import '../styles/components/startYourFreeTrialNowButton.scss'
 import PrimaryButton from './primaryButton'
 
 const StartYourFreeTrialNowButton = () => {
