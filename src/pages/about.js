cb51eb65d8aa36ad590780a85b345b9fe99c5e6e
昌维 <867597730@qq.com>
2020-12-09 18:29:56 +0800

    update about page, replace bulma button component to new Button component

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import AroundParticles from '../components/particles'
 import { graphql, Link } from 'gatsby'
-import { Button } from '@seagreenio/react-bulma'
+import Button from '../components/button'
 
 const About = ({ data }) => {
   const { aboutHeroSVG, timeLineSVG, mobileTimeLineSVG } = data
@@ -52,11 +52,7 @@ const About = ({ data }) => {
               ). TiDB’s sister project, TiKV, is a cloud-native distributed
               Key-Value store. It is now a CNCF incubating project .
             </p>
-            <Button
-              as={Link}
-              to="/contact-us"
-              className="button is-primary is-rounded"
-            >
+            <Button as={Link} to="/contact-us" className={'contact-us-btn'}>
               CONTACT US
             </Button>
           </div>
