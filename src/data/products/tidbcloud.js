3367b16348994fbf364bda767fd305d2a36633cf
YiniXu9506 <xuyini@pingcap.com>
2021-03-25 11:35:07 +0800

    chore: update content

--- src/data/products/tidbcloud.js
+++ src/data/products/tidbcloud.js
@@ -1,50 +1,53 @@
 import aws from '../../../images/products/tidbcloud/aws.png'
-import f1SVG from '../../../images/products/tidbcloud/full-managed.svg'
-import f2SVG from '../../../images/products/tidbcloud/elastic-scaling.svg'
-import f3SVG from '../../../images/products/tidbcloud/visual-diagnostics-and-monitoring.svg'
-import f4SVG from '../../../images/products/tidbcloud/enterprise-grade-security.svg'
+import f1SVG from '../../../images/products/tidbcloud/htap.svg'
+import f2SVG from '../../../images/products/tidbcloud/visual-diagnostics-and-monitoring.svg'
+import f3SVG from '../../../images/products/tidbcloud/enterprise-grade-security.svg'
+import f4SVG from '../../../images/products/tidbcloud/mysql-compatible.svg'
+import f5SVG from '../../../images/products/tidbcloud/open-souce-community.svg'
 import googleCloud from '../../../images/products/tidbcloud/google-cloud.svg'
-import r1SVG from '../../../images/products/tidbcloud/multi-cloud-support.svg'
-import r2SVG from '../../../images/products/tidbcloud/massive-scale.svg'
-import r3SVG from '../../../images/products/tidbcloud/highly-resilient.svg'
-import r4SVG from '../../../images/products/tidbcloud/always-up-to-date.svg'
+import r1SVG from '../../../images/products/tidbcloud/more-control-over-scaling.svg'
+import r2SVG from '../../../images/products/tidbcloud/high-availability-reliability.svg'
+import r3SVG from '../../../images/products/tidbcloud/scalable-multi-master.svg'
+import r4SVG from '../../../images/products/tidbcloud/high-concurrency.svg'
+import r5SVG from '../../../images/products/tidbcloud/transactional-analytical-workloads.svg'
+import r6SVG from '../../../images/products/tidbcloud/real-time-operational-analytics.svg'
 
 const reasonsData = [
   {
-    name: 'Easy to Scale and Manage',
+    name: 'More Control Over Scaling',
     desc:
-      'Scale and manage your TiDB clusters with a click of a button, through our easy-to-use online management console.',
+      'Scale the performance or storage separately to best suit your business needs',
     placeholder: r1SVG,
   },
   {
     name: 'High Availability and Reliability',
     desc:
-      'Ensure business continuity for your mission critical applications with our ACID compliant and Multi-AZ cluster deployment, built upon the RAFT consensus algorithm.',
+      'Ensure business continuity for your mission critical applications with our ACID-compliant cluster deployment',
     placeholder: r2SVG,
   },
   {
-    name: 'Truly Scalable Multi-Master',
+    name: 'Scalable Multi-Master',
     desc:
-      'Scale without restrictions or worry of bottlenecks, as every TiDB node is read and write capable, allowing you to scale both read and write performance horizontally.',
+      'No more bottlenecks - scale without restrictions with TiDB’s read and write capability on every node',
     placeholder: r3SVG,
   },
   {
-    name: 'High Concurrency with High Performance Queries',
+    name: 'High Concurrency',
     desc:
-      'Get high performance for your most demanding applications with our query optimizer, which delivers quick query response times by optimizing performance by scaling horizontally.',
+      'Have thousands of users query your data at the same time without losing performance',
     placeholder: r4SVG,
   },
   {
     name: 'Transactional and Analytical Workloads in a Single Cloud Database',
     desc:
-      'Eliminate complex and costly data migration between multiple transactional (OLTP) and analytical (OLAP) workloads by having your data automatically stored and synced together in one database.',
-    placeholder: r4SVG,
+      'Eliminate complex data migration between transactional and analytical workloads by storing your data in a single database',
+    placeholder: r5SVG,
   },
   {
     name: 'Real Time Operational Analytics',
     desc:
-      'Give your applications the ability to run analytical queries on real time data with our HTAP (hybrid transactional-analytical processing) capabilities that can execute analytical queries in real-time.',
-    placeholder: r4SVG,
+      'Run real-time analytical queries with our hybrid transactional-analytical processing (HTAP) capabilities',
+    placeholder: r6SVG,
   },
 ]
 
@@ -52,28 +55,34 @@ const featuresData = [
   {
     name: 'Hybrid Transactional and Analytical Processing (HTAP)',
     desc:
-      'Run both transactional and analytical workloads in the same cluster, reducing cost and complexity.',
+      'Run both transactional and analytical workloads in the same cluster, reducing cost and complexity',
     placeholder: f1SVG,
-  },
-  {
-    name: 'Elastic Scaling',
-    desc:
-      'Scale in or out your performance and storage simply with a few clicks, while TiDB automatically redistributes your data.',
-    placeholder: f2SVG,
-    reverse: true,
   },
   {
     name: 'Visual Diagnostics and Monitoring',
     desc:
-      'Troubleshoot your database and SQL queries with an intuitive dashboard and diagnostics tools.',
-    placeholder: f3SVG,
+      'Troubleshoot your database and SQL queries with built-in diagnostics tools',
+    placeholder: f2SVG,
+    reverse: true,
   },
   {
     name: 'Enterprise Grade Security',
     desc:
-      'Secure your data in dedicated networks and machines, with support for encryption both in-flight and at-rest.',
+      'Secure your data in dedicated networks and machines, with support for encryption both in-flight and at-rest',
+    placeholder: f3SVG,
+  },
+  {
+    name: 'MySQL Compatible',
+    desc:
+      'Increase productivity and shorten time-to-market for your applications without the need to rewrite your SQL code',
     placeholder: f4SVG,
     reverse: true,
+  },
+  {
+    name: 'Open Source Community',
+    desc:
+      'DB, the engine behind TiDB Cloud, has been open source since day one with a growing community of 400+ contributors',
+    placeholder: f5SVG,
   },
 ]
 
