569c1b0d48e3e1df53e906e11ea3aab043fd1e46
awxxxxxx <dwaterbear@163.com>
2021-11-08 17:27:14 +0800

    Revert "feat: remove stale jp pricing"
    This reverts commit 22f5f6c316de1cebd305c6fb7d08c6ee4cd91b3a.

--- src/pages/jp/products/tidbcloud/index.js
+++ src/pages/jp/products/tidbcloud/index.js
@@ -3,17 +3,191 @@
 import '../../../../styles/pages/products/tidbCloud.scss'
 
 import { graphql } from 'gatsby'
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import {
   featuresDataJP,
+  logos,
   reasonsDataJP,
 } from '../../../../data/products/tidbcloud'
 
+import { Button } from '@seagreenio/react-bulma'
 import Layout from '../../../../components/layout'
 import SEO from '../../../../components/seo'
+import axios from 'axios'
 import GetStartedWithTiDBCloud from '../../../../components/getStartedWithTiDBCloud'
 import StartYourFreeTrialNowButton from '../../../../components/startYourFreeTrialNowButton'
 import { PricingSection } from '../../../../components/pricing'
+
+const cloudProviders = ['aws', 'googleCloud']
+
+const precision = (num, precision = 12) => {
+  if (num === null || num === undefined) return '-'
+  return +parseFloat(num.toPrecision(precision))
+}
+
+const http = axios.create({
+  baseURL: 'https://download.pingcap.com/data/tidbcloud',
+})
+
+const HourlyNodeUsageInfo = () => {
+  const [cloud, setCould] = useState('aws')
+  const [region, setRegion] = useState('')
+  const [profiles, setProfiles] = useState({
+    aws: null,
+    googleCloud: null,
+  })
+
+  useEffect(() => {
+    async function fetchProfiles() {
+      try {
+        const aws = (await http.get('/aws_profiles.json')).data
+        const googleCloud = (await http.get('/gcp_profiles.json')).data
+        setProfiles({ aws, googleCloud })
+        setRegion(aws.regions && aws.regions[0] ? aws.regions[0].name : '')
+      } catch (error) {
+        console.log('Something wrong:' + error)
+        return
+      }
+    }
+
+    fetchProfiles()
+  }, [])
+
+  const handleCloudClick = (cloud) => () => {
+    setCould(cloud)
+    setRegion(
+      profiles[cloud].regions && profiles[cloud].regions[0]
+        ? profiles[cloud].regions[0].name
+        : ''
+    )
+  }
+
+  const handleRegionChange = (event) => {
+    setRegion(event.target.value)
+  }
+
+  const profile = useMemo(() => profiles[cloud], [profiles, cloud])
+
+  const ProfileTable = () => {
+    const instances = profile.instances || []
+    const availableProfiles = instances.filter((p) =>
+      p.available_regions.map((r) => r.region_name).includes(region)
+    )
+
+    const value = (v) => (v !== null && v !== undefined ? v : '-')
+
+    const TierTableRow = ({ tier, isStriped }) => {
+      if (!tier) return null
+
+      const { profile_name, available_regions, tidb, tikv, tiflash } = tier
+      const availablePrice = available_regions.filter(
+        (p) => p.region_name === region
+      )[0].price
+      const names = {
+        tidb: 'TiDB',
+        tikv: 'TiKV',
+        tiflash: 'TiFlash',
+      }
+
+      const TierCells = ({ name, instance }) => (
+        <>
+          <td>{names[name]}</td>
+          <td>{value(instance.cpu)} vCPU</td>
+          {/* <td>{value(instance.memory_gi)} GiB</td> */}
+          <td>
+            {name === 'tidb' ? '-' : `${value(instance.disks[0].disk_gi)} GiB`}
+          </td>
+          <td>$ {value(availablePrice[name])} /時間あたり</td>
+          <td>$ {precision(availablePrice[name] * 24 * 30)} /カ月あたり</td>
+        </>
+      )
+
+      return (
+        <>
+          <tr className={`${isStriped ? 'has-light-background' : ''}`}>
+            <td rowSpan={`${tiflash ? '3' : '2'}`} className="tier-td">
+              {profile_name}
+            </td>
+            <TierCells name="tikv" instance={tikv} />
+          </tr>
+          <tr className={`${isStriped ? 'has-light-background' : ''}`}>
+            <TierCells name="tidb" instance={tidb} />
+          </tr>
+          {tiflash && (
+            <tr className={`${isStriped ? 'has-light-background' : ''}`}>
+              <TierCells name="tiflash" instance={tiflash} />
+            </tr>
+          )}
+        </>
+      )
+    }
+
+    return (
+      <div className="table-container">
+        <table className="table is-bordered comparison-table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Tier</th>
+              <th>Node</th>
+              <th>CPU</th>
+              {/* <th>Memory</th> */}
+              <th>Storage</th>
+              <th>時間単価/1ノード</th>
+              <th>月単価/1ノード</th>
+            </tr>
+          </thead>
+          <tbody>
+            {availableProfiles.map((p, index) => (
+              <TierTableRow
+                key={p.profile_name}
+                tier={p}
+                isStriped={index % 2}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  return (
+    <div className="tidb-cloud-hourly-usage">
+      <div className="cloud-providers">
+        {cloudProviders.map((c) => (
+          <Button
+            key={c}
+            className="cloud-provider-button"
+            active={cloud === c}
+            onClick={handleCloudClick(c)}
+          >
+            <img
+              className="logo"
+              src={logos[c]}
+              alt={`${c.toUpperCase()} Logo`}
+            />
+          </Button>
+        ))}
+      </div>
+
+      <div className="regions">
+        <span className="label">地域を選択</span>
+        <div className="select">
+          {profile && profile.regions && (
+            <select value={region} onChange={handleRegionChange}>
+              {profile.regions.map((r) => (
+                <option key={r.name} value={r.name}>
+                  {r.display_name}
+                </option>
+              ))}
+            </select>
+          )}
+        </div>
+      </div>
+
+      {profile && region && <ProfileTable />}
+    </div>
+  )
+}
 
 const TiDBCloudPage = ({ data }) => {
   const { TiDBCloudLogoPNG } = data
@@ -125,6 +299,31 @@ const TiDBCloudPage = ({ data }) => {
         </section>
 
         <PricingSection />
+
+        <section className="section section-pricing has-light-background">
+          <div className="container">
+            <h2 className="title section-title">価格設定</h2>
+            <div className="field">
+              <p className="paragraph">
+                TiDB Cloudは、さまざまなクラスター層を提供します。 TiDB / TiKV /
+                TiFlashインスタンスの詳細な価格は次のとおりです。
+              </p>
+              <HourlyNodeUsageInfo />
+              <p className="paragraph">
+                データのバックアップと転送は別
+                <a
+                  className="link-with-underline"
+                  href="https://docs.pingcap.com/tidbcloud/beta/tidb-cloud-billing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  課金
+                </a>
+                する必要があります。 詳細については、請求を 参照 してください。
+              </p>
+            </div>
+          </div>
+        </section>
 
         <GetStartedWithTiDBCloud />
       </main>
