dce030c9165309f3aaff6eb53197aad3f56a1aa4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 19:52:36 +0800

    Delete server directory

--- server/migrations/2020-03-12-041107_create_contributors/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE repos (
-  id INTEGER PRIMARY KEY,
-  of TEXT NOT NULL,
-  contributors TEXT NOT NULL
-);
--- server/src/schema.rs
+++
@@ -1,7 +0,0 @@
-table! {
-    repos (id) {
-        id -> Nullable<Integer>,
-        of -> Text,
-        contributors -> Text,
-    }
-}
